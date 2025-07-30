98d531de6690af2b5c06e896b117a6a62b6bd038
Ethan Hill <ethan.hill1999@outlook.com>
2025-07-30 01:38:00 -0500

    text actually works now yay

--- src/main.cpp
+++ src/main.cpp
@@ -11,131 +11,91 @@
 using namespace bn;
 
 constexpr char alphabet[] = {
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-    '`',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '0',
-    '-',
-    '=',
-    '~',
-    '!',
-    '@',
-    '#',
-    '$',
-    '%',
-    '^',
-    '&',
-    '*',
-    '(',
-    ')',
-    '_',
-    '+',
-    '[',
-    ']',
-    '\\',
-    '{',
-    '}',
-    '|',
-    ';',
-    '\'',
-    ':',
-    '"',
-    ',',
-    '.',
-    '/',
-    '<',
-    '>',
-    '?'};
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+    '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
+    '[', ']', '\\', '{', '}', '|', ';', '\'', ':', '"', ',', '.', '/',
+    '<', '>', '?'};
+
+#include "ge_structs.h"
 
 struct text
 {
     vector<sprite_ptr, 24> letters;
-    int start_x = 0;
-    int start_y = 0;
+    vector_2 start;
 
-    text(const string & value) {
-        for (const char ch : value) {
-            for (const char al : alphabet) {
-                if (ch == al) {
-                    auto letter = spr_font_01.create_sprite(start_x, start_y);
-                    start_x += 8;
+    text(const string<24> &value, vector_2 start_ = {0, 0}) : start(start_)
+    {
+        int current_x = start.x.integer();
+
+        for (const char ch : value)
+        {
+
+            if (ch == ' ')
+            {
+                current_x += 8;
+                continue;
+            }
+
+            int char_index = -1;
+            for (size_t i = 0; i < sizeof(alphabet); ++i)
+            {
+                if (ch == alphabet[i])
+                {
+                    char_index = i;
+                    break;
                 }
+            }
+
+            if (char_index != -1 && letters.size() < letters.max_size())
+            {
+                sprite_ptr letter = sprite_items::spr_font_01.create_sprite(current_x, start.y, char_index);
+                letters.push_back(move(letter));
+                current_x += 8;
             }
         }
     }
-};
 
+    // Method to update text position
+    void set_position(int x, int y)
+    {
+        int current_x = x;
+        for (auto &letter : letters)
+        {
+            letter.set_position(current_x, y);
+            current_x += 8;
+        }
+    }
+
+    // Method to hide/show text
+    void set_visible(bool visible)
+    {
+        for (auto &letter : letters)
+        {
+            letter.set_visible(visible);
+        }
+    }
+};
 
 int main()
 {
     core::init();
 
+    // Create wizard sprite
     v_sprite_ptr wizard(&sprite_items::wizard);
+
+    // Create some example text
+    text hello_text("Hello GBA!", {-40, 0});
 
     while (true)
     {
+        // Move wizard based on input
         wizard.move(
-            {(keypad::right_held() * -1) + (keypad::left_held()),
-             (keypad::down_held() * -1) + (keypad::up_held())});
+            {(keypad::left_held() ? -1 : 0) + (keypad::right_held() ? 1 : 0),
+             (keypad::up_held() ? -1 : 0) + (keypad::down_held() ? 1 : 0)});
 
         v_sprite_ptr::update();
         core::update();
